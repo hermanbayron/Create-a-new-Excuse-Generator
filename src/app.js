149ceb5a1449c3880fd3e82eff1bc3858c346311
hermanbayron <foforito2001gg@gmail.com>
2022-09-27 15:36:37 +0000

    New Generator Excuse

--- src/app.js
+++ src/app.js
@@ -1,4 +1,3 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
@@ -7,5 +6,21 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["the dog", "my granma", "his turtle", "my bird"];
+  let what = ["eat", "pissed", "crushed", "broked"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let randomItem = who[Math.floor(Math.random() * who.length)];
+  console.log(randomItem);
+  let randomItem2 = what[Math.floor(Math.random() * what.length)];
+  console.log(randomItem2);
+  let randomItem3 = when[Math.floor(Math.random() * when.length)];
+  console.log(randomItem3);
+  document.getElementById("excuse").innerHTML =
+    randomItem + " " + randomItem2 + " " + randomItem3;
 };
